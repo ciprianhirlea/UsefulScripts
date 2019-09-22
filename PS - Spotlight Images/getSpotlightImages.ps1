dd0b534f12475dcab9ab7da2086687c97162c0ea
Ciprian Hirlea <hirlea.ciprian@gmail.com>
2019-09-22 16:06:34 +0100

    Increased file size and added check for wallpaper folder

--- PS - Spotlight Images/getSpotlightImages.ps1
+++ PS - Spotlight Images/getSpotlightImages.ps1
@@ -2,7 +2,10 @@
 $location = "$HOME\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets"
 
 # Select images that are over 5KB
-$images = Get-ChildItem $location | Where-Object {$_.length -gt 5000} 
+$images = Get-ChildItem $location | Where-Object {$_.length -gt 40000} 
+
+# Checking if folder exist
+if (!(test-path $HOME\Pictures\Wallpapers\)){New-Item -Path "$HOME\Pictures\Wallpapers\" -ItemType Directory}
 
 # Copy images to a wallpaper folder
 ForEach ($image in $images) {cp $location\$image $HOME\Pictures\Wallpapers\$image.jpg}
